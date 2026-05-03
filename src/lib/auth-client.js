430549560c791f95517e81668ceb8ee3e65308e8
tanvirmahmud9225 <tanvirmahmud9225@gmail.com>
2026-05-03 07:55:10 +0600

    extra

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://summer-essentials-store.vercel.app/"
+    baseURL: process.env.BETTER_AUTH_URL
 })
 
 
